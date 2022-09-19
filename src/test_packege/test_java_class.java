c888251a6be14009c9628d4aaff16b021c7591d6
Alexandr Panin <snake13eyes37@gmail.com>
2022-09-19 16:00:25 +0000

    fixup! new_commint

--- src/test_packege/test_java_class.java
+++ src/test_packege/test_java_class.java
@@ -3,6 +3,6 @@ package test_packege;
 public class test_java_class {
     String guano = "gkjdslg";
     void  getGuano(){
-        System.out.println(guano);
+        System.out.println("not guano");
     }
 }
